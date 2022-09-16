95c367a4f4f15182f1cb54f7b1cae5f9fa778318
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-09-16 11:44:04 -0700

    first

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators 
+{
+    public static void main(String[] args) 
+    {
+    int i = 10;
+    int f = 5;
+    f += i = f;
+    boolean flag = true;
+    flag = !flag;
+    while (i>20) {
+        System.out.println(i++);
+    }
+    System.out.print(i);
+{
+    System.out.print
