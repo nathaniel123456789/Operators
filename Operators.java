4d05a80dcbd138790bddf04fe655b6ca085530e0
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-09-22 11:32:00 -0700

    Arrays hope it works 1

--- Operators.java
+++ Operators.java
@@ -2,18 +2,6 @@ public class Operators
 {
     public static void main(String[] args) 
     {
-        int i = 10;
-        int f = 5;
-        boolean flag = true;
-        flag = !flag;
-        i++;
-        System.out.print(i);
-        System.out.print(f);
-        System.out.print(flag);
-        String string1 = "I am a string";
-        String string2 = string1;
-        String string3 = "I am also a string";
-        boolean stringsequal = string1.equals(string3);
         String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
         for (int indI = 0; indI < strArray.length; indI++) {
             System.out.println(strArray[indI]);
