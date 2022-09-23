776fe2a625b43471736cac203be578b83190c72d
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-09-23 11:11:35 -0700

    Dashes now work with any string length!

--- Operators.java
+++ Operators.java
@@ -2,17 +2,18 @@ public class Operators
 {
     public static void main(String[] args) 
     {
-        String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo","testing"};
         for (int indI = 0; indI < strArray.length; indI++) {
             System.out.println(strArray[indI]);
         }
         for (String value : strArray) {
             System.out.print(value);
         }
+        int anything = strArray.length - 1;
         System.out.println();
         for (int indII = 0; indII < strArray.length; indII++) {
             System.out.print(strArray[indII]);
-            if (indII < 4) 
+            if (indII < anything) 
                 System.out.print("-");
             }
         }
